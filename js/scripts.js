909fa9408b650aa6541e99a97dc98a0a1409fe62
Ebru Rice <ebruerenb@gmail.com>
2021-11-06 10:29:24 -0700

    Completed JavaScript code

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,16 @@ $(document).ready(function() {
     const meaning = parseInt($("input:radio[name=meaning]:checked").val());
     const time = parseInt($("input:radio[name=time]:checked").val());
     const pizza = parseInt($("input:radio[name=pizza]:checked").val());
+    const total = coffee + fictional + meaning + time + pizza;
+    $("#language").show();
+
+    if (total<=5) {
+      $("#result").text("C#");
+    }
+      else if (10>total>5) {
+      $("#result").text("JavaScript")
+    } else {
+      $("#result").text("Phyton")
+    }
   });
 });
