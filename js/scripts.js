92d7c3f43921514971a29c662c00fb53628d4788
Ebru Rice <ebruerenb@gmail.com>
2021-11-06 10:13:13 -0700

    Define JavaScript variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
   $("#submit").click(function() {
-    const 
+    const coffee = parseInt($("input:radio[name=coffee]:checked").val());
+    const fictional = parseInt($("input:radio[name=fictional]:checked").val());
+    const meaning = parseInt($("input:radio[name=meaning]:checked").val());
+    const time = parseInt($("input:radio[name=time]:checked").val());
+    const pizza = parseInt($("input:radio[name=pizza]:checked").val());
   });
 });
